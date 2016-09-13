d9224a33984cdb19ecf3c33a19da024c32eda286
Doug <git@digitalfx.me.uk>
2016-09-13 10:33:29 +0100

    Replaces VVOpenSource Xcode project with VVBasics, VVBufferPool and VVISFKit frameworks.

--- vvopensource/ISFonIOSTest/ISFonIOSTestAppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <VVBufferPool/VVBufferPool.h>
-
-@interface ISFonIOSTestAppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-
-@end
--- vvopensource/MTCSender/MTCMIDIManager.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <VVMIDI/VVMIDI.h>
-
-@interface MTCMIDIManager : VVMIDIManager
-
-@end
